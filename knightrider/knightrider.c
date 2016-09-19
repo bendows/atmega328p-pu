4d59b43c56d15217fe87492670e779dac44693b0
Ben Potgieter <ben.potgieter@viamedia.co.za>
2016-09-19 21:18:37 +0200

    funflash

--- knightrider/knightrider.c
+++ knightrider/knightrider.c
@@ -26,12 +26,37 @@ while(count--) {
 */
 void main (void)
 {
+	char i=0;
  DDRD = 0b11111111;
  while(1) {
 	 for (PORTD = 0x01; PORTD != 0; PORTD <<= 1) 
-		 _delay_ms(70);
+		 _delay_ms(200);
 	 for (PORTD = 0x80; PORTD != 0; PORTD >>= 1) 
-		 _delay_ms(70);
-
+		 _delay_ms(200);
+	 for (PORTD = 0x01; PORTD != 0; PORTD <<= 1) 
+		 _delay_ms(200);
+	 for (PORTD = 0x80; PORTD != 0; PORTD >>= 1) 
+		 _delay_ms(200);
+	 if (i>3) {
+		i=0;
+		continue;
+	}
+	 i++;
+	 PORTD = 0x11;
+		 _delay_ms(100);
+	 PORTD = 0x22;
+		 _delay_ms(100);
+	 PORTD = 0x33;
+		 _delay_ms(100);
+	 PORTD = 0x44;
+		 _delay_ms(100);
+	 PORTD = 0x55;
+		 _delay_ms(100);
+	 PORTD = 0x66;
+		 _delay_ms(100);
+	 PORTD = 0x77;
+		 _delay_ms(100);
+	 PORTD = 0x88;
+		 _delay_ms(400);
  }
 }
