ded5b5bdfc831bcbd5337f35c4a38de4faeefa8c
Ben Potgieter <ben.potgieter@pricecheck.co.za>
2019-09-12 23:41:32 +0200

    ok

--- README.mk
+++ README.mk
@@ -1,5 +1,8 @@
 # STANDOLINE ATMEGA328P-PU
 
+apt install avrdude
+apt install gcc-avr
+
 VCC = +5V
 
 VCC - R 10k   -	1	28
@@ -74,7 +77,155 @@ GND 		GND	RX0
 
 	ICSP HEADER HERE
 
+On the NANO maybe need a 10Uf electrolytic between reset and GND on the VIN pins side
+# dmesg -T
+
+[Thu Sep 12 22:57:40 2019] usb 3-1: USB disconnect, device number 8
+[Thu Sep 12 22:57:40 2019] ftdi_sio ttyUSB0: FTDI USB Serial Device converter now disconnected from ttyUSB0
+[Thu Sep 12 22:57:40 2019] ftdi_sio 3-1:1.0: device disconnected
+[Thu Sep 12 22:57:42 2019] usb 3-1: new full-speed USB device number 9 using xhci_hcd
+[Thu Sep 12 22:57:42 2019] usb 3-1: New USB device found, idVendor=0403, idProduct=6001
+[Thu Sep 12 22:57:42 2019] usb 3-1: New USB device strings: Mfr=1, Product=2, SerialNumber=3
+[Thu Sep 12 22:57:42 2019] usb 3-1: Product: FT232R USB UART
+[Thu Sep 12 22:57:42 2019] usb 3-1: Manufacturer: FTDI
+[Thu Sep 12 22:57:42 2019] usb 3-1: SerialNumber: A7030WNQ
+[Thu Sep 12 22:57:42 2019] ftdi_sio 3-1:1.0: FTDI USB Serial Device converter detected
+[Thu Sep 12 22:57:42 2019] usb 3-1: Detected FT232RL
+[Thu Sep 12 22:57:42 2019] usb 3-1: FTDI USB Serial Device converter now attached to ttyUSB0
+
+apt-get install avrdude
+apt-get install gcc-avr
+
+
+root@ben-Vostro-3560:/home/ben# avrdude -vvvV -c stk500v1 -p ATMEGA328P -P /dev/ttyUSB0 -b 19200
+
+avrdude: Version 6.2
+         Copyright (c) 2000-2005 Brian Dean, http://www.bdmicro.com/
+         Copyright (c) 2007-2014 Joerg Wunsch
+
+         System wide configuration file is "/etc/avrdude.conf"
+         User configuration file is "/root/.avrduderc"
+         User configuration file does not exist or is not a regular file, skipping
+
+         Using Port                    : /dev/ttyUSB0
+         Using Programmer              : stk500v1
+         Overriding Baud Rate          : 19200
+         AVR Part                      : ATmega328P
+         Chip Erase delay              : 9000 us
+         PAGEL                         : PD7
+         BS2                           : PC2
+         RESET disposition             : dedicated
+         RETRY pulse                   : SCK
+         serial program mode           : yes
+         parallel program mode         : yes
+         Timeout                       : 200
+         StabDelay                     : 100
+         CmdexeDelay                   : 25
+         SyncLoops                     : 32
+         ByteDelay                     : 0
+         PollIndex                     : 3
+         PollValue                     : 0x53
+         Memory Detail                 :
+
+                                  Block Poll               Page                       Polled
+           Memory Type Mode Delay Size  Indx Paged  Size   Size #Pages MinW  MaxW   ReadBack
+           ----------- ---- ----- ----- ---- ------ ------ ---- ------ ----- ----- ---------
+           eeprom        65    20     4    0 no       1024    4      0  3600  3600 0xff 0xff
+                                  Block Poll               Page                       Polled
+           Memory Type Mode Delay Size  Indx Paged  Size   Size #Pages MinW  MaxW   ReadBack
+           ----------- ---- ----- ----- ---- ------ ------ ---- ------ ----- ----- ---------
+           flash         65     6   128    0 yes     32768  128    256  4500  4500 0xff 0xff
+                                  Block Poll               Page                       Polled
+           Memory Type Mode Delay Size  Indx Paged  Size   Size #Pages MinW  MaxW   ReadBack
+           ----------- ---- ----- ----- ---- ------ ------ ---- ------ ----- ----- ---------
+           lfuse          0     0     0    0 no          1    0      0  4500  4500 0x00 0x00
+                                  Block Poll               Page                       Polled
+           Memory Type Mode Delay Size  Indx Paged  Size   Size #Pages MinW  MaxW   ReadBack
+           ----------- ---- ----- ----- ---- ------ ------ ---- ------ ----- ----- ---------
+           hfuse          0     0     0    0 no          1    0      0  4500  4500 0x00 0x00
+                                  Block Poll               Page                       Polled
+           Memory Type Mode Delay Size  Indx Paged  Size   Size #Pages MinW  MaxW   ReadBack
+           ----------- ---- ----- ----- ---- ------ ------ ---- ------ ----- ----- ---------
+           efuse          0     0     0    0 no          1    0      0  4500  4500 0x00 0x00
+                                  Block Poll               Page                       Polled
+           Memory Type Mode Delay Size  Indx Paged  Size   Size #Pages MinW  MaxW   ReadBack
+           ----------- ---- ----- ----- ---- ------ ------ ---- ------ ----- ----- ---------
+           lock           0     0     0    0 no          1    0      0  4500  4500 0x00 0x00
+                                  Block Poll               Page                       Polled
+           Memory Type Mode Delay Size  Indx Paged  Size   Size #Pages MinW  MaxW   ReadBack
+           ----------- ---- ----- ----- ---- ------ ------ ---- ------ ----- ----- ---------
+           calibration    0     0     0    0 no          1    0      0     0     0 0x00 0x00
+                                  Block Poll               Page                       Polled
+           Memory Type Mode Delay Size  Indx Paged  Size   Size #Pages MinW  MaxW   ReadBack
+           ----------- ---- ----- ----- ---- ------ ------ ---- ------ ----- ----- ---------
+           signature      0     0     0    0 no          3    0      0     0     0 0x00 0x00
+
+         Programmer Type : STK500
+         Description     : Atmel STK500 Version 1.x firmware
+         Hardware Version: 2
+         Firmware Version: 1.18
+         Topcard         : Unknown
+         Vtarget         : 0.0 V
+         Varef           : 0.0 V
+         Oscillator      : Off
+         SCK period      : 0.1 us
+
+avrdude: AVR device initialized and ready to accept instructions
+
+Reading | ################################################## | 100% 0.05s
+
+avrdude: Device signature = 0x1e950f (probably m328p)
+avrdude: safemode read 1, lfuse value: e2
+avrdude: safemode read 2, lfuse value: e2
+avrdude: safemode read 3, lfuse value: e2
+avrdude: safemode: lfuse reads as E2
+avrdude: safemode read 1, hfuse value: d9
+avrdude: safemode read 2, hfuse value: d9
+avrdude: safemode read 3, hfuse value: d9
+avrdude: safemode: hfuse reads as D9
+avrdude: safemode read 1, efuse value: ff
+avrdude: safemode read 2, efuse value: ff
+avrdude: safemode read 3, efuse value: ff
+avrdude: safemode: efuse reads as FF
+
+avrdude: safemode read 1, lfuse value: e2
+avrdude: safemode read 2, lfuse value: e2
+avrdude: safemode read 3, lfuse value: e2
+avrdude: safemode: lfuse reads as E2
+avrdude: safemode read 1, hfuse value: d9
+avrdude: safemode read 2, hfuse value: d9
+avrdude: safemode read 3, hfuse value: d9
+avrdude: safemode: hfuse reads as D9
+avrdude: safemode read 1, efuse value: ff
+avrdude: safemode read 2, efuse value: ff
+avrdude: safemode read 3, efuse value: ff
+avrdude: safemode: efuse reads as FF
+avrdude: safemode: Fuses OK (E:FF, H:D9, L:E2)
+
+avrdude done.  Thank you.
+
 Example make output:
+
+
+# pwd
+# /home/ben
+# avrdude -V -c stk500v1 -p ATMEGA328P -P /dev/ttyUSB0 -b 19200
+# avrdude -V -c stk500v1 -p ATMEGA328P -P /dev/ttyUSB0 -b 19200
+avrdude: ser_open(): can't open device "/dev/ttyUSB0": Permission denied
+avrdude done.  Thank you.
+# sudo su
+[sudo] password for ben:
+# avrdude -V -c stk500v1 -p ATMEGA328P -P /dev/ttyUSB0 -b 19200
+
+avrdude: AVR device initialized and ready to accept instructions
+
+Reading | ################################################## | 100% 0.05s
+
+avrdude: Device signature = 0x1e950f (probably m328p)
+
+avrdude: safemode: Fuses OK (E:FF, H:D9, L:E2)
+
+avrdude done.  Thank you.
 
 make flash CPU_SPEED=8000000
 
