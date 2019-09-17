89bc5111f4eb91508540a952c54fe94c9c4cd8a8
Ben Potgieter <ben.potgieter@pricecheck.co.za>
2019-09-18 01:09:38 +0200

    double flash

--- knightrider/knightrider.c
+++ knightrider/knightrider.c
@@ -3,55 +3,31 @@
  
 #define ON_DELAY_MS 30
 #define OFF_DELAY_MS 1000
-/* 
-void flash (void) {
-  PORTD = 0b01010101;
-  _delay_ms(ON_DELAY_MS);
-  PORTD = 0b10101010;
-  _delay_ms(ON_DELAY_MS);
-  PORTD = 0b00000000;
-}
 
-void delay_ms(uint16_t count) {
- while(count--) {
-      _delay_ms(1);
-      }
-}
+	unsigned char i=0;
+void main (void) {
+ 	DDRD = 0b11111111;
+	while(1) {
+	for (PORTD = 0x1; PORTD != 0; PORTD <<= 1) 	//KnightRider
+		 _delay_ms(150);
+	 for (PORTD = 0x80; PORTD != 0; PORTD >>= 1) 	//KnightRider
+		 _delay_ms(150);
 
-void delay_us(uint16_t count) {
-while(count--) {
-      _delay_us(1);
-   }
-}
-*/
-void main (void)
-{
-	char i=0;
- DDRD = 0b11111111;
- while(1) {
-	 /*
-	 for (PORTD = 0x01; PORTD != 0; PORTD <<= 1) 
-		 _delay_ms(110);
-		 */
-	 PORTD = 0b01010101;
-		 _delay_ms(1000);
-	 for (PORTD = 0x80; PORTD != 0; PORTD >>= 1) 
-		 _delay_ms(1000);
-	 PORTD = 0b01010101;
-		 _delay_ms(1000);
-	 for (PORTD = 0x01; PORTD != 0; PORTD <<= 1) 
-		 _delay_ms(1000);
-	 /*
-	 PORTD = 0b10101010;
-		 _delay_ms(110);
-	 PORTD = 0b01010101;
-		 _delay_ms(110);
-	 for (PORTD = 0x80; PORTD != 0; PORTD >>= 1) 
-		 _delay_ms(110);
-	 PORTD = 0b10101010;
-		 _delay_ms(110);
-	 PORTD = 0b01010101;
-		 _delay_ms(110);
-		 */
+	//DOUBLE FLASH
+	PORTD = 0; _delay_ms(250); PORTD = 0b10101010; _delay_ms(150);
+	PORTD = 0; _delay_ms(250); PORTD = 0b10101010; _delay_ms(150);
+	PORTD = 0; _delay_ms(250);
+
+	for (PORTD = 1; PORTD != 0; PORTD <<= 1) 	//KnightRider
+		 _delay_ms(150);
+	 for (PORTD = 128; PORTD != 0; PORTD >>= 1) 	//KnightRider
+		 _delay_ms(150);
+	//DOUBLE FLASH
+	PORTD = 0; _delay_ms(250); PORTD = 0b10101010; _delay_ms(150);
+	PORTD = 0; _delay_ms(250); PORTD = 0b10101010; _delay_ms(150);
+	PORTD = 0; _delay_ms(250);
+	for (PORTD=0;PORTD<255;PORTD++) { 		//Count from 0 to 255
+		 _delay_ms(50);
+	}
  }
 }
