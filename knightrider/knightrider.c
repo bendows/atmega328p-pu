f81323fe77e8b9b5c9c59b291c1f9ed3af5a2729
bendows <ben@updated.co.za>
2020-01-28 22:32:36 +0000

    Update knightrider.c

--- knightrider/knightrider.c
+++ knightrider/knightrider.c
@@ -1,33 +1,32 @@
 #include <avr/io.h>
 #include <util/delay.h>
- 
+
 #define ON_DELAY_MS 30
 #define OFF_DELAY_MS 1000
-
-	unsigned char i=0;
-void main (void) {
- 	DDRD = 0b11111111;
-	while(1) {
-	for (PORTD = 0x1; PORTD != 0; PORTD <<= 1) 	//KnightRider
-		 _delay_ms(150);
-	 for (PORTD = 0x80; PORTD != 0; PORTD >>= 1) 	//KnightRider
-		 _delay_ms(150);
-
-	//DOUBLE FLASH
-	PORTD = 0; _delay_ms(250); PORTD = 0b10101010; _delay_ms(150);
-	PORTD = 0; _delay_ms(250); PORTD = 0b10101010; _delay_ms(150);
-	PORTD = 0; _delay_ms(250);
-
-	for (PORTD = 1; PORTD != 0; PORTD <<= 1) 	//KnightRider
-		 _delay_ms(150);
-	 for (PORTD = 128; PORTD != 0; PORTD >>= 1) 	//KnightRider
-		 _delay_ms(150);
-	//DOUBLE FLASH
-	PORTD = 0; _delay_ms(250); PORTD = 0b10101010; _delay_ms(150);
-	PORTD = 0; _delay_ms(250); PORTD = 0b10101010; _delay_ms(150);
-	PORTD = 0; _delay_ms(250);
-	for (PORTD=0;PORTD<255;PORTD++) { 		//Count from 0 to 255
-		 _delay_ms(250);
-	}
+void main (void)
+{
+ DDRD = 0b11111111;
+ while(1) {
+         for (PORTD = 0x80; PORTD != 0; PORTD >>= 1)
+                 _delay_ms(220);
+         for (PORTD = 0x01; PORTD != 0; PORTD <<= 1)
+                 _delay_ms(220);
+         PORTD = 0x0;
+                 _delay_ms(1000);
+         PORTD = 255;
+                 _delay_ms(1000);
+         PORTD = 0;
+                 _delay_ms(1000);
+         PORTD = 0b01010101;
+                 _delay_ms(1000);
+         PORTD = 0b10101010;
+                 _delay_ms(1000);
+         for (PORTD = 0; PORTD < 255; PORTD++)
+                 _delay_ms(120);
+        _delay_ms(1000);
+         PORTD = 0;
+        _delay_ms(1000);
+         PORTD = 255;
+        _delay_ms(1000);
  }
 }
